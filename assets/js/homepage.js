aa58d1ee9ab54d709a70044856d453cda7960acc
Noctus <jonnyfaulkner21@gmail.com>
2021-06-08 17:53:59 -0500

    added ability so find specific user repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,12 @@
 var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
+      console.log(data);
+    });
+  });
 };
 
-getUserRepos();
+var user = "JonnyFaulkner"
+
+getUserRepos(user);
